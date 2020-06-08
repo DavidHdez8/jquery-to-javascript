7cb87e9dbe1837d91d812653069011bae77f4cab
DavidHdez8 <david-0610@live.com.mx>
2020-06-08 01:28:30 -0500

    Promises

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,38 @@
 console.log('hola mundo!');
-const noCambia = "Leonidas";
+const noCambia = "David";
 
-let cambia = "@LeonidasEsteban"
+let cambia = "@DavidHdez8";
 
 function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
+  cambia = nuevoNombre;
 }
+
+const promises = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve("Promise resolved");
+  }, 5000);
+});
+
+const getUsers = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    reject();
+  },2000)
+})
+
+promises
+  .then(function(msg){
+    console.log(msg);
+  })
+  .catch(function(msg){
+    console.error(msg);
+  })
+
+Promise.all([
+  getUsers
+])
+  .then(function(){
+    console.log("Promise Resolved");
+  })
+  .catch(function(){
+    console.error("Reject in promise");
+  })
