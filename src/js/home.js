016d4367a0851389048b60e0b4e3107795603001
DavidHdez8 <david-0610@live.com.mx>
2020-06-11 02:17:59 -0500

    Visibilidad del modal y programacion de oculto de la misma

--- src/js/home.js
+++ src/js/home.js
@@ -66,8 +66,10 @@ fetch('https://randomuser.me/api/')
   }
 
   const $form = document.getElementById('form');
+  const $home = document.getElementById('home');
   $form.addEventListener('submit', () => {
     event.preventDefault();
+    $home.classList.add('search-active');
   })
   const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
   const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
@@ -93,7 +95,7 @@ fetch('https://randomuser.me/api/')
 
   function addEventClick($element){
     $element.addEventListener('click', () => {
-      alert('AUCH!!');
+      showModal();
     });
   }
 
@@ -122,8 +124,6 @@ fetch('https://randomuser.me/api/')
 
 
 const $featuringContainer = document.getElementById('#featuring');
-const $form = document.getElementById('#form');
-const $home = document.getElementById('#home');
 
 const $modal = document.getElementById('modal');
 const $overlay = document.getElementById('overlay');
@@ -132,3 +132,13 @@ const $hideModal = document.getElementById('hide-modal');
 const $modalTitle = $modal.querySelector('h1');
 const $modalImage = $modal.querySelector('img');
 const $modalDescription = $modal.querySelector('p');
+
+function showModal() {
+  $overlay.classList.add('active');
+  $modal.style.animation = 'modalIn .8s forwards';
+}
+
+$hideModal.addEventListener('click', () => {
+  $overlay.classList.remove('active');
+  $modal.style.animation = 'modalOut .8s forwards';
+});
