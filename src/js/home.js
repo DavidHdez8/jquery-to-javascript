be035b8e8f9a51d0ceefef450e79e977687bdac2
DavidHdez8 <david-0610@live.com.mx>
2020-06-12 19:39:27 -0500

    Formulario

--- src/js/home.js
+++ src/js/home.js
@@ -74,7 +74,27 @@ fetch('https://randomuser.me/api/')
       $element.setAttribute(attribute, attributes[attribute]);
     }
   }
-  $form.addEventListener('submit', () => {
+
+  function featuringTemplate(peli){
+    return(
+      `
+      <div class="home-featuring" id="featuring">
+      <div class="featuring">
+        <div class="featuring-image">
+          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${peli.title}</p>
+        </div>
+      </div>
+    </div>
+    `
+    );
+  }
+  const BASE_API = 'https://yts.mx/api/v2/';
+
+  $form.addEventListener('submit', async (event) => {
     event.preventDefault();
     $home.classList.add('search-active');
     const $loader = document.createElement('img');
@@ -84,10 +104,15 @@ fetch('https://randomuser.me/api/')
       width: 50,
     })
     $featuringContainer.append($loader);
+
+    const data = new FormData($form);
+    const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`); //Peticiones web
+    const HTMLString = featuringTemplate(peli.data.movies[0]);
+    $featuringContainer.innerHTML = HTMLString;
   })
-  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
-  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
-  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
+  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`);
+  const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`);
+  const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`);
   console.log(actionList, dramaList, animationList);
 
   function videoItemTemplates (movie){
